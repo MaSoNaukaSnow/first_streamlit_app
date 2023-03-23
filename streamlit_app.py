9505e04bdb6c0b11be039b14bfd97164e109c060
MaSoNaukaSnow <128613627+MaSoNaukaSnow@users.noreply.github.com>
2023-03-23 14:55:58 +0100

    Clean up the code 1

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,7 @@ import streamlit
 import pandas
 import requests
 import snowflake.connector
+from urllib.error import URLError
 
 streamlit.title('New Healthy Diner')
 streamlit.header('Breakfast Menu')
@@ -21,17 +22,19 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 # Display the table on the page.
 streamlit.dataframe(fruits_to_show)
 
-#Fruitvice
+#Fruitvice API response
 streamlit.header("Fruityvice Fruit Advice!")
-#get what user wants
-fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
-streamlit.write('The user entered ', fruit_choice)  
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
-#normalize response
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-#show on screen
-streamlit.dataframe(fruityvice_normalized)
-
+try:
+  fruit_choice = streamlit.text_input('What fruit would you like information about?')
+  if not fruit_choice:
+    streamlit.error("Please select a fruit to get informaiton.")
+  else: 
+    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+    streamlit.dataframe(fruityvice_normalized)
+except URLError as e:
+    streamlit.error()
+    
 #Snowflake check
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
